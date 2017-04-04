cb3097e47c2883c9a3f3e85e80cb94ca7542c634
renadope <renadoparia@gmail.com>
2017-04-04 13:41:56 -0400

    Test Push

---
+++ app/src/main/java/com/dcit_remake/comp3275_group_project/Course.java
@@ -0,0 +1,11 @@
+package com.dcit_remake.comp3275_group_project;
+
+/**
+ * Created by Renado_Paria on 4/4/2017 at 1:38 PM for DcitApp.
+ */
+
+class Course
+{
+    private String courseID;
+    private String courseName;
+}
