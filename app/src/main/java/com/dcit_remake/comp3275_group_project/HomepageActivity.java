863a7baeaf300bc5f46de7dcf914e7bba37b2129
Mickel McMillan <mickelmcmillan@gmail.com>
2017-04-04 21:28:14 -0500

    Designed landing activity, placed buttons on the homepage to go the different programmes and created the individual programme activities (IT and CompSci) with the navigation drawer

--- app/src/main/java/com/dcit_remake/comp3275_group_project/HomepageActivity.java
+++ app/src/main/java/com/dcit_remake/comp3275_group_project/HomepageActivity.java
@@ -1,7 +1,9 @@
 package com.dcit_remake.comp3275_group_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void goToIT(View view){
+        Intent intent = new Intent(getApplicationContext(), InformationTechnologyActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCompSci(View view){
+        Intent intent = new Intent(getApplicationContext(), ComputerScienceActivity.class);
+        startActivity(intent);
     }
 }
